5bc083c3b5def3b8b7c35a942a8bcb4c0168014e
vadim-rm <vadimrm01@gmail.com>
2024-12-07 23:49:47 +0300

    fix: display account as cards instead of table

---
+++ src/components/AccountCard.tsx
@@ -0,0 +1,81 @@
+import {Link} from "react-router-dom";
+import {HandlerGetAccountsListAccount} from "../api/Api.ts";
+import {FC} from "react";
+import {accountStatuses} from "../api/constants.ts";
+import {useIsModerator} from "../slices/user.ts";
+import "./AccountCard.css"
+
+interface Props extends HandlerGetAccountsListAccount {
+    onChangeStatus: (id: number, status: string) => void
+}
+
+const AccountCard: FC<Props> = ({
+                                    id,
+                                    number,
+                                    creator,
+                                    status,
+                                    createdAt,
+                                    requestedAt,
+                                    totalFee,
+                                    onChangeStatus,
+                                }) => {
+    const formatDate = (dateString: string) => {
+        const date = new Date(dateString)
+        return date.toLocaleDateString('ru-RU', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+        })
+    }
+    const isModerator = useIsModerator()
+
+    return (
+        <div className="account-card-info">
+            <Link className="title" to={`/accounts/${id}`}>
+                Cчёт №{number}
+            </Link>
+            <div className="account description-row">
+                <div className="account-item-badge">
+                    <p className="digit">{accountStatuses[status!]}</p>
+                    <p className="description">статус</p>
+                </div>
+
+                <div className="account-item-badge">
+                    <p className="digit">{formatDate(createdAt!)}</p>
+                    <p className="description">дата создания</p>
+                </div>
+
+                <div className="account-item-badge">
+                    <p className="digit">{formatDate(requestedAt!)}</p>
+                    <p className="description">дата подачи</p>
+                </div>
+
+                <div className="account-item-badge">
+                    <p className="digit">{totalFee} ₽</p>
+                    <p className="description">ежемесячный платеж</p>
+                </div>
+
+                <div className="account-item-badge">
+                    <p className="digit">{creator}</p>
+                    <p className="description">создатель</p>
+                </div>
+            </div>
+            <div className="buttons-row">
+                <Link to={`/accounts/${id}`} className="card-button secondary">
+                    Открыть
+                </Link>
+                {(isModerator && status === 'applied') &&
+                    <>
+                        <td>
+                            <Link onClick={() => onChangeStatus(id!, 'rejected')}>Отклонить</Link>
+                        </td>
+                        <td>
+                            <Link onClick={() => onChangeStatus(id!, 'finalized')}>Выполнить</Link>
+                        </td>
+                    </>}
+            </div>
+        </div>
+    );
+};
+
+export default AccountCard;
--- src/pages/AccountsPage.tsx
+++ src/pages/AccountsPage.tsx
@@ -2,17 +2,13 @@ import Layout from "../components/Layout.tsx";
 import {FC, useCallback, useEffect, useState} from "react";
 import {ROUTE_LABELS} from "../Routes.ts";
 import {BreadCrumbs} from "../components/BreadCrumbs.tsx";
-import {Form, Spinner, Table} from "react-bootstrap";
+import {Form, Spinner} from "react-bootstrap";
 import {HandlerGetAccountsListAccount} from "../api/Api.ts";
 import {api} from "../api";
-import {Link} from "react-router-dom";
-import {accountStatuses} from "../api/constants.ts";
 import "./AccountsPage.css"
-import {useIsModerator} from "../slices/user.ts";
+import AccountCard from "../components/AccountCard.tsx";
 
 const AccountsPage: FC = () => {
-    const isModerator = useIsModerator()
-
     const [accounts, setAccounts] = useState<HandlerGetAccountsListAccount[]>([])
     const [status, setStatus] = useState("any")
     const [loading, setLoading] = useState(true)
@@ -32,13 +28,12 @@ const AccountsPage: FC = () => {
 
     useEffect(() => {
         load()
-        const id = setTimeout(load, 2000);
-        return () => clearTimeout(id);
+        const id = setInterval(load, 2000);
+        return () => clearInterval(id);
     }, [load])
 
     const filterAccounts = () => accounts.filter((account) =>
         account.creator!.toLowerCase().includes(creator.toLowerCase()))
-
 
     const changeStatus = (id: number, status: string) => {
         api.accounts.completeUpdate(id, {
@@ -46,11 +41,6 @@ const AccountsPage: FC = () => {
         }).then(() => {
             load()
         })
-    }
-
-    const formatDate = (dateString: string) => {
-        const date = new Date(dateString)
-        return date.toLocaleString()
     }
 
     return (
@@ -89,44 +79,9 @@ const AccountsPage: FC = () => {
             {loading && <div className="loadingBg"><Spinner animation="border"/></div>}
             {!loading && accounts.length === 0 && <div>Нет заявок на счета</div>}
             {!loading && accounts.length > 0 &&
-                <Table striped bordered hover>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Создатель</th>
-                        <th>Статус</th>
-                        <th>Дата создания</th>
-                        <th>Дата подачи</th>
-                        <th>Ежемесячный платеж</th>
-                        <th colSpan={isModerator ? 3 : 1}>Действия</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {filterAccounts().map((account, index) => (
-                        <tr key={index}>
-                            <td>{account.id}</td>
-                            <td>{account.creator}</td>
-                            <td>{accountStatuses[account.status!]}</td>
-                            <td>{formatDate(account.createdAt!)}</td>
-                            <td>{formatDate(account.requestedAt!)}</td>
-                            <td>{account.totalFee}</td>
-                            <td colSpan={isModerator && account.status === 'applied' ? 1 : 3}>
-                                <Link to={`/accounts/${account.id}`}>Открыть</Link>
-                            </td>
-                            {(isModerator && account.status === 'applied') &&
-                                <>
-                                    <td>
-                                        <Link onClick={() => changeStatus(account.id!, 'rejected')}>Отклонить</Link>
-                                    </td>
-                                    <td>
-                                        <Link onClick={() => changeStatus(account.id!, 'finalized')}>Выполнить</Link>
-                                    </td>
-                                </>
-                            }
-                        </tr>
-                    ))}
-                    </tbody>
-                </Table>}
+                filterAccounts().map((account, index) => (
+                    <AccountCard key={index} {...account} onChangeStatus={changeStatus}/>
+                ))}
         </Layout>
     )
 }
